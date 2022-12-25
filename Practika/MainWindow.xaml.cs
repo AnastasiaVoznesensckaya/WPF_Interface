f4a398ad12920733d7ada896103b2f68665b0e3a
AnastasiaVoznesensckaya <aanastasia.voznesensckaja@gmail.com>
2022-12-26 03:58:18 +0500

    WPF

---
+++ Practika/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practika
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void MenuItem_Click_info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Автор: Вознесенская Анастасия", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void MenuItem_Click_exit(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_Red(object sender, RoutedEventArgs e)
+        {
+           Window.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        private void MenuItem_Click_Yellow(object sender, RoutedEventArgs e)
+        {
+            Window.Background = new SolidColorBrush((Color)Colors.Yellow);
+        }
+
+        private void MenuItem_Click_Gray(object sender, RoutedEventArgs e)
+        {
+            Window.Background = new SolidColorBrush(Colors.Gray);
+        }
+
+        private void MenuItem_Click_Blue(object sender, RoutedEventArgs e)
+        {
+            Window.Background= new SolidColorBrush((Color)Colors.Blue);
+        }
+
+        private void MenuItem_Click_Orange(object sender, RoutedEventArgs e)
+        {
+            Window.Background = new SolidColorBrush(Colors.Orange);
+        }
+
+        private void ToolBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Bar.Text = "Элемент управления: ToolBar";
+        }
+
+        private void ToolBar_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Bar.Text = "";
+        }
+
+        private void Menu_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Bar.Text = "Элемент управления: Menu";
+        }
+
+        private void Menu_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Bar.Text = "";
+        }
+    }
+}
---
+++ Praktika2/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktika2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CanvaName.EditingMode = InkCanvasEditingMode.None;
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            TextSize.Text = slide.Value.ToString();
+            if (CanvaName == null) return;
+            CanvaName.DefaultDrawingAttributes.Width = slide.Value;
+            CanvaName.DefaultDrawingAttributes.Height = slide.Value;
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            CanvaName.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+
+        private void ComboBoxItem_Selected_Green(object sender, RoutedEventArgs e)
+        {
+           CanvaName.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+
+        private void ComboBoxItem_Selected_Blue(object sender, RoutedEventArgs e)
+        {
+            CanvaName.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+
+        private void ComboBoxItem_Selected_Yellow(object sender, RoutedEventArgs e)
+        {
+            CanvaName.DefaultDrawingAttributes.Color = Colors.Yellow;
+        }
+
+        private void ComboBoxItem_Selected_Black(object sender, RoutedEventArgs e)
+        {
+            CanvaName.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+
+        private void ComboBoxItem_Selected_Brown(object sender, RoutedEventArgs e)
+        {
+            CanvaName.DefaultDrawingAttributes.Color = Colors.Brown;
+        }
+
+        private void RadioButton_Boot2_Click(object sender, RoutedEventArgs e)
+        {
+            CanvaName.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        }
+
+        private void RadioButton_Boot3_Click(object sender, RoutedEventArgs e)
+        {
+            CanvaName.Strokes.Clear();
+        }
+
+        private void RadioButton_Boot1_Click(object sender, RoutedEventArgs e)
+        {
+            CanvaName.EditingMode = InkCanvasEditingMode.InkAndGesture;
+        }
+    }
+}
